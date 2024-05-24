880b9c957e63236732147093c33c21850f2a8155
Your Name <you@example.com>
2024-05-24 11:15:50 +0200

    merge branch main => chemin à fix

---
+++ handle.go
@@ -0,0 +1,365 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/sessions"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+// Clé de chiffrement pour les cookies de sessio
+var store = sessions.NewCookieStore([]byte("keySession"))
+
+// Configuration de la durée d'expiration du cookie (par exemple, 40 secondes)
+const sessionExpiration = 1 * time.Hour
+
+type RegisterHandler struct {
+	db        *sql.DB
+	dbInitErr error
+}
+
+type User struct {
+	ID          int
+	Username    string
+	Email       string
+	Image       []byte
+	Base64Image string
+}
+
+type App struct {
+	config *oauth2.Config
+}
+
+type Goauth struct {
+	Id            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+}
+
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/login/oauth" {
+		if r.Method == "GET" {
+			url := a.config.AuthCodeURL("state", oauth2.AccessTypeOffline)
+			http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+		}
+	}
+	if r.URL.Path == "/callback" {
+		if r.Method == "GET" {
+			code := r.URL.Query().Get("code")
+
+			t, err := a.config.Exchange(context.Background(), code)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			client := a.config.Client(context.Background(), t)
+			resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+
+			var jsonResp Goauth
+
+			if err = json.NewDecoder(resp.Body).Decode(&jsonResp); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+		}
+	}
+}
+func (u *User) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	session, err := store.Get(r, "userSession")
+	if err != nil {
+		log.Printf("Erreur lors de la récupération de la session: %v", err)
+		http.Error(w, "Erreur de session", http.StatusInternalServerError)
+		return
+	}
+	if r.URL.Path == "/" {
+		if r.Method == "GET" {
+			tmpl, err := template.ParseFiles("index.html")
+			if err != nil {
+				http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+				return
+			}
+			tmpl.Execute(w, u)
+			return
+		}
+	}
+	if r.URL.Path == "/register" {
+		if r.Method == "GET" {
+			tmpl, err := template.ParseFiles("signin.html")
+			if err != nil {
+				http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+				return
+			}
+			tmpl.Execute(w, nil)
+			return
+		} else if r.Method == "POST" {
+			u.processRegistration(w, r)
+			return
+		}
+	}
+	if r.URL.Path == "/login" {
+		if r.Method == "GET" {
+			tmpl, err := template.ParseFiles("login.html")
+			if err != nil {
+				http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+				return
+			}
+			tmpl.Execute(w, u)
+			return
+		} else if r.Method == "POST" {
+			u.processLogin(w, r)
+			return
+		}
+	}
+
+	if r.URL.Path == "/profile" {
+		if r.Method == "GET" {
+			if userID, ok := session.Values["userID"].(int); ok {
+				u.loadUserFromDB(userID)
+				u.handleUser(w, r)
+				return
+			}
+
+		} else if r.Method == "POST" {
+			fmt.Println("demande de deco")
+			http.Redirect(w, r, "/logout", http.StatusSeeOther)
+			return
+		}
+	}
+	if r.URL.Path == "/logout" {
+		if r.Method == "GET" {
+			for key := range session.Values {
+				delete(session.Values, key)
+			}
+			session.Options.MaxAge = -1
+
+			// Sauvegarder la session pour appliquer les changements
+			err = session.Save(r, w)
+			if err != nil {
+				http.Error(w, "Erreur lors de la sauvegarde de la session", http.StatusInternalServerError)
+				return
+			}
+			u.processLogout(w, r)
+
+			return
+		}
+		if r.Method == "POST" {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+/* func (h *RegisterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.db, h.dbInitErr = sql.Open("sqlite3", "./forumv3.db")
+	if h.dbInitErr != nil {
+		http.Error(w, "Erreur de base de données", http.StatusInternalServerError)
+		return
+	}
+
+	if r.URL.Path == "/" {
+		if r.Method == "GET" {
+			tmpl, err := template.ParseFiles("register.html")
+			if err != nil {
+				http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+				return
+			}
+			tmpl.Execute(w, nil)
+			return
+		} else if r.Method == "POST" {
+			h.processRegistration(w, r)
+			return
+		}
+	}
+	http.NotFound(w, r)
+} */
+
+func (u *User) processLogin(w http.ResponseWriter, r *http.Request) {
+	db, dbInitErr := sql.Open("sqlite3", "./forumv3.db")
+	if dbInitErr != nil {
+		http.Error(w, "Erreur de base de données", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	r.ParseMultipartForm(10 << 20) // 10 MB max file size
+
+	email := r.FormValue("emaild")
+	password := r.FormValue("passwordd")
+	err := db.QueryRow("SELECT user_id, username, email FROM users WHERE email=? AND password=?", email, password).Scan(&u.ID, &u.Username, &u.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Aucun utilisateur trouvé avec cet ID", http.StatusNotFound)
+			return
+		}
+		log.Printf("Erreur lors de la récupération des informations de l'utilisateur: %v", err)
+		http.Error(w, "Erreur lors de la récupération des informations de l'utilisateur", http.StatusInternalServerError)
+		return
+	}
+
+	// Sauvegarder l'ID de l'utilisateur dans la session
+	session, err := store.Get(r, "userSession")
+	if err != nil {
+		log.Printf("Erreur lors de la récupération de la session: %v", err)
+		http.Error(w, "Erreur de session", http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["userID"] = u.ID
+	fmt.Println("Logged in user ID:", session.Values["userID"])
+
+	// Définir l'expiration de la session
+	session.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   int(sessionExpiration.Seconds()), // Durée en secondes
+		HttpOnly: true,                             // Pour des raisons de sécurité
+	}
+
+	err = session.Save(r, w)
+	if err != nil {
+		log.Printf("Erreur lors de la sauvegarde de la session: %v", err)
+		http.Error(w, "Erreur lors de la sauvegarde de la session", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/profile", http.StatusSeeOther)
+}
+
+func (u *User) processRegistration(w http.ResponseWriter, r *http.Request) {
+	db, dbInitErr := sql.Open("sqlite3", "./forumv3.db")
+	if dbInitErr != nil {
+		http.Error(w, "Erreur de base de données", http.StatusInternalServerError)
+		return
+	}
+
+	r.ParseMultipartForm(10 << 20) // 10 MB max file size
+
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	file, _, err := r.FormFile("profile_picture")
+	if err != nil {
+		http.Error(w, "Erreur lors de l'obtention du fichier", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// Lire le fichier dans un slice de bytes
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		http.Error(w, "Erreur lors de la lecture du fichier", http.StatusInternalServerError)
+		return
+	}
+	fileBytes := buf.Bytes()
+
+	// Insérer l'utilisateur dans la base de données
+	stmt, err := db.Prepare("INSERT INTO users(username, email, password, profile_picture) VALUES(?, ?, ?, ?)")
+	if err != nil {
+		http.Error(w, "Erreur lors de la préparation de la requête", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, email, password, fileBytes)
+	if err != nil {
+		http.Error(w, "Erreur lors de l'exécution de la requête", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func (u *User) loadUserFromDB(userID int) {
+	db, dbInitErr := sql.Open("sqlite3", "./forumv3.db")
+	if dbInitErr != nil {
+		log.Printf("Erreur de base de données: %v", dbInitErr)
+		return
+	}
+	defer db.Close()
+
+	err := db.QueryRow("SELECT username, email, profile_picture FROM users WHERE user_id=?", userID).Scan(&u.Username, &u.Email, &u.Image)
+	if err != nil {
+		log.Printf("Erreur lors de la récupération des informations de l'utilisateur: %v", err)
+	}
+}
+
+func (u *User) handleUser(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("usertest.html")
+	if err != nil {
+		log.Printf("Erreur de serveur: %v", err)
+		http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+		return
+	}
+	data := struct {
+		Username    string
+		Email       string
+		Image       []byte
+		Base64Image string
+	}{
+		Username:    u.Username,
+		Email:       u.Email,
+		Image:       u.Image,
+		Base64Image: base64.StdEncoding.EncodeToString(u.Image),
+	}
+	tmpl.Execute(w, data)
+}
+
+func (u *User) processLogout(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("logouttest.html")
+	if err != nil {
+		log.Printf("Erreur de serveur: %v", err)
+		http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+		return
+
+	}
+	tmpl.Execute(w, nil)
+
+}
+func main() {
+	clientId := "205949073068-pgfqbsm6h9bahpgaq505o8k7a7iidhcr.apps.googleusercontent.com"
+	clientSecret := "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"
+
+	conf := &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  "http://localhost:5500/callback",
+		Scopes:       []string{"email", "profile"},
+		Endpoint:     google.Endpoint,
+	}
+
+	app := App{config: conf}
+
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
+
+	http.Handle("/", new(User))
+	http.Handle("/register", new(User))
+	http.Handle("/profile", new(User))
+	http.Handle("/login", new(User))
+	http.Handle("/login/oauth", &app)
+	http.Handle("/callback", &app)
+	http.Handle("/logout", new(User))
+
+	log.Fatal(http.ListenAndServe(":5500", nil))
+}
---
+++ public/script.js
@@ -0,0 +1,51 @@
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+var addPostBtn = document.getElementById("add-button");
+var addPostModal = document.getElementById("add-post-modal");
+var closeModal = document.querySelector("#add-post-modal .close");
+var filtersBtn = document.getElementById("filters-button");
+var filtersModal = document.getElementById("filters-modal");
+var closeFiltersModal = document.querySelector("#filters-modal .close");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+addPostBtn.onclick = openAdd;
+closeModal.onclick = closeAdd;
+filtersBtn.onclick = openFilters;
+closeFiltersModal.onclick = closeFilters;
+
+function openNav() {
+    sidenav.classList.add("active");
+    openBtn.style.display = "none";
+}
+
+function closeNav() {
+    sidenav.classList.remove("active");
+    openBtn.style.display = "block";
+}
+
+function openAdd() {
+    addPostModal.classList.add("show");
+}
+
+function closeAdd() {
+    addPostModal.classList.remove("show");
+}
+
+function openFilters() {
+    filtersModal.classList.add("show");
+}
+
+function closeFilters() {
+    filtersModal.classList.remove("show");
+}
+
+window.onclick = function(event) {
+    if (event.target == addPostModal) {
+        closeAdd();
+    }
+    if (event.target == filtersModal) {
+        closeFilters();
+    }
+}
